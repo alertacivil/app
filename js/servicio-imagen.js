2a2e8977dff01f89eebc84eb7867633a9b7cb88b
alfernandez26 <afernandezg1@ucenfotec.ac.cr>
2021-02-24 19:00:19 -0600

    categoria victima

---
+++ js/servicio-imagen.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const boton_foto = document.querySelector('#btn-foto');
+const imagen = document.querySelector('#user-photo');
+
+let widget_cloudinary = cloudinary.createUploadWidget({
+    cloudName : 'civilalert',
+    uploadPreset : 'civilalert_preset'
+
+}, (err, result) => {
+    if (!err && result && result.event === 'success') {
+        console.log('Imagen subida con éxito', result.info);
+        imagen.src = result.info.secure_url;
+    }
+});
+
+boton_foto.addEventListener('click', () => {
+    widget_cloudinary.open();
+}, false);
