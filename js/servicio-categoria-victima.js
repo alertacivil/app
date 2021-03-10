2494e30dfaaa176cb62cf7f420cfe2af795a9758
alfernandez26 <afernandezg1@ucenfotec.ac.cr>
2021-03-09 20:44:19 -0600

    categoria victima

--- js/servicio-categoria-victima.js
+++ js/servicio-categoria-victima.js
@@ -54,16 +54,16 @@ let listar_categoria_victima = async() => {
     return lista_categoriavictima;
 };
 
-let obtener_categoria_victima = async(categoria_incidente) => {
-    try {
-        const response = await axios({
-            method: 'get',
-            params: { categoria_incidente: categoria_incidente },
-            url: `http://localhost:3000/api/buscar-categoriaincidente`,
-            responseType: 'json'
-        });
-        return response.data.cIncidente;
-    } catch (error) {
-        console.log(error);
-    }
-};
+// let obtener_categoria_victima = async(categoria_incidente) => {
+//     try {
+//         const response = await axios({
+//             method: 'get',
+//             params: { categoria_incidente: categoria_incidente },
+//             url: `http://localhost:3000/api/buscar-categoriaincidente`,
+//             responseType: 'json'
+//         });
+//         return response.data.cIncidente;
+//     } catch (error) {
+//         console.log(error);
+//     }
+// };
