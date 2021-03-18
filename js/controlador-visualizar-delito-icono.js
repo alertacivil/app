3d1c6f0b8262a10f7eee4adbb17845a4f11b49e1
alfernandez26 <afernandezg1@ucenfotec.ac.cr>
2021-03-17 20:12:22 -0600

    box

--- js/controlador-visualizar-delito-icono.js
+++ js/controlador-visualizar-delito-icono.js
@@ -18,65 +18,15 @@ window.initMap = function() {
       center: new google.maps.LatLng(9.933, -84.08),
 
     });
-// // //a partir de aqui search box
-//     const input = document.getElementById("pac-input");
-//     const searchBox = new google.maps.places.SearchBox(input);
-//     map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
-
-//     map.addListener("bounds_changed", () => {
-//       searchBox.setBounds(map.getBounds());
-//     });
-//     let markers = [];
-//     // Listen for the event fired when the user selects a prediction and retrieve
-//     // more details for that place.
-//     searchBox.addListener("places_changed", () => {
-//       const places = searchBox.getPlaces();
-  
-//       if (places.length == 0) {
-//         return;
-//       }
-//       // Clear out the old markers.
-//       markers.forEach((marker) => {
-//         marker.setMap(null);
-//       });
-//       // markers = [];
-//       // // For each place, get the icon, name and location.
-//       // const bounds = new google.maps.LatLngBounds();
-//       // places.forEach((place) => {
-//       //   if (!place.geometry || !place.geometry.location) {
-//       //     console.log("Returned place contains no geometry");
-//       //     return;
-//       //   }
-//       //   const icon = {
-//       //     url: place.icon,
-//       //     size: new google.maps.Size(71, 71),
-//       //     origin: new google.maps.Point(0, 0),
-//       //     anchor: new google.maps.Point(17, 34),
-//       //     scaledSize: new google.maps.Size(25, 25),
-//       //   };
-//       //   // Create a marker for each place.
-//       //   markers.push(
-//       //     new google.maps.Marker({
-//       //       map,
-//       //       icon,
-//       //       title: place.name,
-//       //       position: place.geometry.location,
-//       //     })
-//       //   );
-  
-//       //   if (place.geometry.viewport) {
-//       //     // Only geocodes have viewport.
-//       //     bounds.union(place.geometry.viewport);
-//       //   } else {
-//       //     bounds.extend(place.geometry.location);
-//       //   }
-//       // });
-//       // map.fitBounds(bounds);
-//     });
-
-//final search box
-
      let mostrar_mapa = async() => {
+      const input = document.getElementById("pac-input");
+      const autocomplete = new google.maps.places.Autocomplete(input);
+      autocomplete.bindTo("bounds", map);
+      // Specify just the place data fields that you need.
+      autocomplete.setFields(["place_id", "geometry", "name"]);
+      map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+
+
       lista_puntos = await listar_delito(); 
       lista_tipovictima = await listar_categoria_victima();
       
